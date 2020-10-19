35aafc484db0c0fe1dbc3695c33f0e5877e7e68b
chendi04 <chendi04@baidu.com>
2020-10-19 15:00:39 +0800

    饱和运算

---
+++ chapter2/2-73.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <limits.h>
+#include <assert.h>
+
+int saturating_add(int x, int y) {
+    int sum = x + y;
+    int sig_mask = INT_MIN;
+    /*
+    * if x > 0, y > 0 but sum < 0, it's a positive overflow
+    * if x < 0, y < 0 but sum >= 0, it's a negetive overflow
+    * thanks for https://dreamanddead.gitbooks.io/csapp-3e-solutions/content/chapter2/2.73.html
+    */
+    //x和y的符号位为0，并且sum的符号位为1，这种情况可以判断是正溢出
+    int pos_over = !(x & sig_mask) && !(y & sig_mask) && (sum & sig_mask);
+    //x和y的符号位为1，并且sum的符号位为0，这种情况可以判断是负溢出
+    int neg_over = (x & sig_mask) && (y & sig_mask) && !(sum & sig_mask);
+
+    (pos_over && (sum = INT_MAX) || neg_over && (sum = INT_MIN));
+
+    return sum;
+}
+
+int main(int argc, char* argv[]) {
+  assert(INT_MAX == saturating_add(INT_MAX, 0x1234));
+  assert(INT_MIN == saturating_add(INT_MIN, -0x1234));
+  assert(0x11 + 0x22 == saturating_add(0x11, 0x22));
+  return 0;
+}
+
