abcafd8204ec19cb422f1f5e4297a2e8b72630d1
chendi04 <chendi04@baidu.com>
2020-10-21 18:55:23 +0800

    二进制串的推导

--- chapter2/2-83.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("设x=0.yyyyy   \n");
-    printf("x<<y就是y.yyyy 其中y是k位序列  Y=B2U(y)\n");
-    printf("所以x*2^k = Y + x \n");
-    printf("所以x = Y / (2^k -1)) \n");
-}
