6e465dc0e9b4592062f9ab8a2e3b6dfe92a0c562
chendi04 <chendi04@baidu.com>
2020-10-26 15:38:39 +0800

    位级浮点编码操作

--- chapter2/2-93.c
+++ chapter2/2-93.c
@@ -2,7 +2,7 @@
 
 typedef unsigned float_bits;
 
-float_bits float_negate(float_bits f) {
+float_bits float_absval(float_bits f) {
     unsigned sign = f>>31;
     unsigned exp = f>>23 & 0xFF;
     unsigned frac = f & 0x7FFFFF;
@@ -10,8 +10,9 @@ float_bits float_negate(float_bits f) {
         // NAN. return f
         return f;
     }
+    (sign == 1) && (sign = 0);
     // reassemble bits
-    return (~sign << 31) | (exp << 23) | frac;
+    return (sign << 31) | (exp << 23) | frac;
 }
 
 int main() {
@@ -19,13 +20,13 @@ int main() {
     unsigned y;
     x = 0xfff01234; //nan
     printf("%f \n",*(float *)&x);
-    printf("%u \n",float_negate(x));
+    printf("%u \n",float_absval(x));
     x = 0x3f000000;//+0.5f
     printf("%f \n",*(float *)&x);
-    y = float_negate(x);
+    y = float_absval(x);
     printf("%f \n",*(float *)&y);
     x = 0xbf000000;//-0.5f
     printf("%f \n",*(float *)&x);
-    y = float_negate(x);
+    y = float_absval(x);
     printf("%f \n",*(float *)&y);
 }
